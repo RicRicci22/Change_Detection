86e9fe7345660e6561afa785c9ceec586d762243
ricricci22 <riccardo.ricci-1@unitn.it>
2023-09-20 15:41:21 +0200

    push

--- blip2_only_method.py
+++
@@ -1,8 +0,0 @@
-from utils.chat import *
-from PIL import Image
-import os
-import json
-from tqdm import tqdm
-from utils.images_utils import extract_blobs, get_largest_poly, crop_image
-import numpy as np
-import matplotlib.pyplot as plt
